d4a8b47308304b290ee6179ff109c42339733ff3
Zeeshan Syed <zsyed89@gmail.com>
2020-11-05 19:30:06 -0500

    Created collection-item, collection-review components, and shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
