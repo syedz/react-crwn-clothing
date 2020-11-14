d21353af5195415017c7cb643d56b65759b2e584
Zeeshan Syed <zsyed89@gmail.com>
2020-11-14 10:38:46 -0500

    Refactored Google signin to use sagas

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { googleSignInStart } from '../../redux/user/user.actions';
 
 import './sign-in.styles.scss';
 
@@ -39,6 +41,8 @@ class SignIn extends React.Component {
     }
 
     render() {
+        const { googleSignInStart } = this.props;
+
         return (
             <div className="sign-in">
                 <h2>I already have an account</h2>
@@ -65,7 +69,11 @@ class SignIn extends React.Component {
                         <CustomButton type="submit">
                             Sign in
                         </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton 
+                            type="button" 
+                            onClick={googleSignInStart} 
+                            isGoogleSignIn
+                        >
                             Sign in with Google
                         </CustomButton>
                     </div>
@@ -75,4 +83,8 @@ class SignIn extends React.Component {
     }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
