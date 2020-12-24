109e175a0fff667e3885eaff3f622bcbd43cb259
Zeeshan Syed <zsyed89@gmail.com>
2020-12-24 17:32:22 -0500

    Created ErrorBoundary component

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 
-import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+import Spinner from '../spinner/spinner.component';
 
 // Higher order component
 // Takes component as an argument, and returns another component
-const WithSpinner = WrappedComponent => {
-    const Spinner = ({ isLoading, ...otherProps }) => {
-        return isLoading ? (
-            <SpinnerOverlay>
-                <SpinnerContainer />
-            </SpinnerOverlay>
-        ) : (
-            <WrappedComponent {...otherProps} />
-        );
-    }
-    return Spinner;
+const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
+        <Spinner />
+    ) : (
+        <WrappedComponent {...otherProps} />
+    );
 };
 
 export default WithSpinner;
